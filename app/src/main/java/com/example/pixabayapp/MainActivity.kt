5f3b476e3e31a495427bc8e1549d266d63ae2c0d
mozhdeh.nouri <mozhdehnouri@yahoo.com>
2024-06-30 10:39:49 +0330

    Init Project (#Master)

---
+++ app/src/main/java/com/example/pixabayapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.pixabayapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pixabayapp.ui.theme.PixabayAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PixabayAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        repositories {
+                google()
+                mavenCentral()
+                gradlePluginPortal()
+            }
+        }
+    }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PixabayApp"
+include(":app")
+ 
